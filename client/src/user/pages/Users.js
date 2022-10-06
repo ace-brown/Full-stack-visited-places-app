0ea0b48ceb873d49918f2e98fdb73a830e04411a
KAD105 <kazem_abdi@yahoo.com>
2022-10-06 08:05:18 +0200

    refactor: header

--- client/src/user/pages/Users.js
+++ client/src/user/pages/Users.js
@@ -1,11 +1,17 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return (
-    <div>
-      <h1>Users</h1>
-    </div>
-  );
+  const USERS = [
+    {
+      id: "u1",
+      name: "John",
+      image:
+        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      place: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
