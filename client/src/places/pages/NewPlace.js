889532227f046112df9d339e9eb228bbf8bd2ed2
KAD105 <kazem_abdi@yahoo.com>
2022-10-07 07:20:55 +0200

    add: new page for new place and styles for that

--- client/src/places/pages/NewPlace.js
+++ client/src/places/pages/NewPlace.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React from 'react';
+
+import Input from '../../shared/components/FormElements/Input';
+import './NewPlace.css';
 
 const NewPlace = () => {
   return (
-    <div>
-      <h1>NewPlace</h1>
-    </div>
+    <form className="place-form">
+      <Input element="input" type="text" label="Title" />
+    </form>
   );
 };
 
