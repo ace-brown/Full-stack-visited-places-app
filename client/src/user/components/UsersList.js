3e55e60f7223a19495fa07735951e18d16e15ef2
KAD105 <kazem_abdi@yahoo.com>
2022-10-12 07:26:27 +0200

    add: sending first fetch request for login and signup

--- client/src/user/components/UsersList.js
+++ client/src/user/components/UsersList.js
@@ -22,7 +22,7 @@ const UsersList = (props) => {
           id={user.id}
           image={user.image}
           name={user.name}
-          placeCount={user.place}
+          placeCount={user.places.length}
         />
       ))}
     </ul>
--- server/app.js
+++ server/app.js
@@ -10,8 +10,18 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/api/places", placesRoutes);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
+  next();
+});
+
+app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
 app.use((req, res) => {
@@ -31,7 +41,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://fullstack_visited_place_app:TcZGruCxEqUCopik@cluster0.zo6mc77.mongodb.net/places?retryWrites=true&w=majority"
+    "mongodb+srv://fullstack_visited_place_app:TcZGruCxEqUCopik@cluster0.zo6mc77.mongodb.net/mern?retryWrites=true&w=majority"
   )
   .then(app.listen(5000))
   .catch((err) => {
